c4e258e06849ad913d28aa7a31571387a13f08d0
Przemysław Moskal <ptmoskal@gmail.com>
2018-10-18 23:06:25 +0200

    Added new classes needed to provide functionality of exploring exam results.

--- app/src/main/java/com/ptmprojects/aplikacjaegzaminatora/MainActivity.java
+++ app/src/main/java/com/ptmprojects/aplikacjaegzaminatora/MainActivity.java
@@ -6,23 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
     public static final String TAG = "MainActivity";
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "MainActivity.onCreate()");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = new MainFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new MainFragment();
     }
 }
