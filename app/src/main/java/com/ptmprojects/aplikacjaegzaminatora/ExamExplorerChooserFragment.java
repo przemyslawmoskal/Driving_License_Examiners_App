cf4313f2f6c77c434c285ce31017a4a390c11c76
Przemysław Moskal <ptmoskal@gmail.com>
2018-10-19 20:43:09 +0200

    Added sample code to check if ExamExplorerChooserFragment can show another Fragment in the same Activity.

--- app/src/main/java/com/ptmprojects/aplikacjaegzaminatora/ExamExplorerChooserFragment.java
+++ app/src/main/java/com/ptmprojects/aplikacjaegzaminatora/ExamExplorerChooserFragment.java
@@ -4,15 +4,42 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class ExamExplorerChooserFragment extends Fragment {
+    private static final String DEFAULT_CHOOSE_DATE_RANGE_SPINNER_VALUE = "Dzisiaj";
+    private Spinner mChooseDateRangeSpinner;
+    private Button mSearchButton;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_exam_explorer_chooser, container, false);
+
+        mChooseDateRangeSpinner = v.findViewById(R.id.date_range_chooser_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.time_scopes, android.R.layout.simple_spinner_item);
+        int spinnerDefaultPosition = adapter.getPosition(DEFAULT_CHOOSE_DATE_RANGE_SPINNER_VALUE);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mChooseDateRangeSpinner.setAdapter(adapter);
+        mChooseDateRangeSpinner.setSelection(spinnerDefaultPosition);
+
+        mSearchButton = (Button) v.findViewById(R.id.showExamsButton);
+        mSearchButton.setOnClickListener((view) -> {
+            Fragment fragment = new ExamExplorerListFragment();
+            FragmentManager fm = getActivity().getSupportFragmentManager();
+            fm.beginTransaction()
+                    .add(R.id.detail_fragment_container, fragment)
+                    .commit();
+        });
+
         return v;
     }
 }
--- app/src/main/java/com/ptmprojects/aplikacjaegzaminatora/SingleFragmentActivity.java
+++ app/src/main/java/com/ptmprojects/aplikacjaegzaminatora/SingleFragmentActivity.java
@@ -15,22 +15,6 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected int getLayoutResId() {
         return R.layout.activity_fragment;
     }
-//
-//    @Override
-//    protected void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(getLayoutResId());
-//
-//        FragmentManager fm = getSupportFragmentManager();
-//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-//        if (fragment == null) {
-//            fragment = createFragment();
-//            fm.beginTransaction()
-//                    .add(R.id.fragment_container, fragment)
-//                    .commit();
-//        }
-//    }
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
